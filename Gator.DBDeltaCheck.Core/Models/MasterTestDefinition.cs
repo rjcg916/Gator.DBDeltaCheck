d3203e40b8fc940dda9d16464f42e1f8aac68024
Bob Graham <rgraham@wcirb.com>
2025-07-23 13:57:48 -0700

    tweaks

--- Gator.DBDeltaCheck.Core/Models/MasterTestDefinition.cs
+++ Gator.DBDeltaCheck.Core/Models/MasterTestDefinition.cs
@@ -2,27 +2,21 @@
 
 namespace Gator.DBDeltaCheck.Core.Models;
 
-
 public class MasterTestDefinition
 {
     /// <summary>
     /// A descriptive name for the test case, used for logging and test runner output.
     /// </summary>
-    [JsonProperty("testName")]
     public string TestName { get; set; }
 
     [JsonIgnore]
     public string DefinitionFilePath { get; set; }
 
-    [JsonProperty("arrange")]
     public ArrangeDefinition Arrange { get; set; }
 
-    [JsonProperty("act")]
     public ActionDefinition Action { get; set; }
 
-    [JsonProperty("assert")]
     public AssertDefinition Assert { get; set; }
 
-    [JsonProperty("teardown")]
     public TeardownDefinition Teardown { get; set; } 
 }
