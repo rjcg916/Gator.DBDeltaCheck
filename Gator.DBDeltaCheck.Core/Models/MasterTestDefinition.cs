babe1a3c8abe09a3ba7f17ea17bacbd6ed7006e6
Bob Graham <rgraham@wcirb.com>
2025-08-14 15:57:40 -0700

    cleanup

--- Gator.DBDeltaCheck.Core/Models/MasterTestDefinition.cs
+++ Gator.DBDeltaCheck.Core/Models/MasterTestDefinition.cs
@@ -4,17 +4,17 @@ namespace Gator.DBDeltaCheck.Core.Models;
 
 public class MasterTestDefinition
 {
+    // Properties that must exist in the JSON
     public required string TestCaseName { get; set; }
+    public required List<StepInstruction> Arrange { get; init; }
+    public required List<StepInstruction> Actions { get; init; }
+    public required List<ExpectedStateAssertion> Assert { get; init; }
+    public required List<StepInstruction> Teardown { get; init; }
 
-    [JsonProperty("DataMapFile")] public string? DataMapFile { get; set; }
+    // Optional property in the JSON (can be null)
+    public string? DataMapFile { get; init; }
 
-    [JsonIgnore] public required string DefinitionFilePath { get; set; }
-
-    public required List<StepInstruction> Arrange { get; set; }
-
-    public required List<StepInstruction> Actions { get; set; }
-
-    public required List<ExpectedStateAssertion> Assert { get; set; }
-
-    public required List<StepInstruction> Teardown { get; set; }
+    // A property NOT from the JSON that MUST be set manually after deserialization
+    [JsonIgnore]
+    public required string DefinitionFilePath { get; set; }
 }
